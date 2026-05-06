f8665df5aa33b2bf839760bf4719a29767dd4864
Praveen Sharma <80846168+praveendesharma@users.noreply.github.com>
2026-05-06 13:36:17 -0700

    added code with errors

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 def main():
     print("Hello from dsc190-assignment05!")
 
+a = 0
+if a == 0:\n        a = 1\n\tb = 1
+
+if True:
+   a = 1
+
+items = [1, 2, 3]
+for item in items:
+pass
 
 if __name__ == "__main__":
     main()
