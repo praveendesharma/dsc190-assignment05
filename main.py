e064db56923e538bc7f74bed96ed8e11c54734cf
Praveen Sharma <80846168+praveendesharma@users.noreply.github.com>
2026-05-06 14:06:48 -0700

    fixed errors

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ def main():
     print("Hello from dsc190-assignment05!")
 
 a = 0
-if a == 0:\n        a = 1\n\tb = 1
+if a == 0:
+    a = 1
+    b = 1
 
 if True:
-   a = 1
+    a = 1
 
 items = [1, 2, 3]
 for item in items:
-pass
+    pass
 
 if __name__ == "__main__":
     main()
